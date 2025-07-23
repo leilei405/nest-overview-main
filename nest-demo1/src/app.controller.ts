b1d627572aa9363550e937b669e0b0292a1bc485
leilei405 <1601178425@qq.com>
2025-07-24 00:28:17 +0800

    feat: ✨ 接口版本控制

--- nest-demo1/src/app.controller.ts
+++ nest-demo1/src/app.controller.ts
@@ -10,7 +10,7 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- nest-demo1/src/main.ts
+++ nest-demo1/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api'); // 全局路由前缀
+  /** 全局路由前缀 */
+  app.setGlobalPrefix('api');
+  /** 版本控制 */
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
