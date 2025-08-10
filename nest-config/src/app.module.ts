60978092b11609d2a90d38d4d92dcee0d07d5747
leilei405 <1601178425@qq.com>
2025-08-10 12:46:55 +0800

    refactor(nest-config): 移除DB_HOST的IP验证并注释掉

--- nest-config/src/app.module.ts
+++ nest-config/src/app.module.ts
@@ -19,7 +19,7 @@ import { envFilePath } from './utils/env';
           .default('development'),
         // DB_PORT: Joi.number().default(3000), // 默认数据库端口
         DB_PORT: Joi.number().valid(3306, 3307).default(3306), // 校验数据库端口 只能是 3306 或 3307
-        DB_HOST: Joi.string().ip({ version: ['ipv4', 'ipv6'] }), // 默认数据库主机
+        // DB_HOST: Joi.string().ip(),
         DB_URL: Joi.string().domain(),
       }),
     }),
